9e8ff21d9f58120d6395385a3695540b7b18f939
Kseniya Vasileva <vasilieva.kss@gmail.com>
2024-04-03 11:15:23 +0400

    added compontn Question

--- src/comonents/StartScreen.js
+++ src/comonents/StartScreen.js
@@ -1,9 +1,14 @@
-function StartScreen({ numQuestion }) {
+function StartScreen({ numQuestion, dispatch }) {
   return (
     <div className="start">
       <h2>Welcome to The React Quiz</h2>
       <h3>{numQuestion} questions to test your React mastery</h3>
-      <button className="btn btn-ui">Let's start</button>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "start" })}
+      >
+        Let's start
+      </button>
     </div>
   );
 }
